b909ad366d57e41eff3a10900f000fd43d5e41bb
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-06-28 22:34:59 +0200

    Create models.py

---
+++ project_quotes/quotes/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+class Author(models.Model):
+    fullname = models.CharField(max_length=120, null=False)
+    born_date = models.CharField(max_length=120)
+    born_location = models.CharField(max_length=120)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.fullname}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Quote(models.Model):
+    quote = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.quote}"
