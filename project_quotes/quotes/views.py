80e825ceb47c92184c1a381e0867e89cedaf19eb
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-06-30 10:28:51 +0200

    Update views.py

--- project_quotes/quotes/views.py
+++ project_quotes/quotes/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .forms import AuthorForm, QuoteForm, TagForm
+
+from .models import Quote, Tag, Author
 
 
 # Create your views here.
 def main(request):
-    return render(request, 'quotes/index.html')
+    quotes = Quote.objects.filter(user=request.user).all() if request.user.is_authenticated else []
+    return render(request, 'quotes/index.html', {"quotes": quotes})
 
 
+@login_required
 def add_tag(request):
     if request.method == 'POST':
         form = TagForm(request.POST)
         if form.is_valid():
-            form.save()
+            tag = form.save(commit=False)
+            tag.user = request.user
+            tag.save()
             return redirect(to='quotes:main')
         else:
             return render(request, 'quotes/tag.html', {'form': form})
@@ -19,11 +26,14 @@ def add_tag(request):
     return render(request, 'quotes/tag.html', {'form': TagForm()})
 
 
+@login_required
 def add_author(request):
     if request.method == 'POST':
         form = AuthorForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_author = form.save(commit=False)
+            new_author.user = request.user
+            new_author.save()
             return redirect(to='quotes:main')
         else:
             return render(request, 'quotes/author.html', {'form': form})
@@ -31,13 +41,19 @@ def add_author(request):
     return render(request, 'quotes/author.html', {'form': AuthorForm()})
 
 
+@login_required
 def add_quote(request):
     if request.method == 'POST':
         form = QuoteForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_quote = form.save(commit=False)
+            new_quote.user = request.user
+            new_quote.save()
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'), user=request.user)
+            choice_author = Author.objects.filter(name__in=request.POST.getlist('author'), user=request.user)
             return redirect(to='quotes:main')
         else:
             return render(request, 'quotes/quote.html', {'form': form})
 
     return render(request, 'quotes/quote.html', {'form': QuoteForm()})
+
