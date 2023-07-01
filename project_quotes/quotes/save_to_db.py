1e65722cd7991446fa2a434a812b2621cfea5a0b
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-02 00:40:17 +0200

    Update save_to_db.py

--- project_quotes/quotes/save_to_db.py
+++ project_quotes/quotes/save_to_db.py
@@ -5,13 +5,12 @@ from django.core.exceptions import MultipleObjectsReturned
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_quotes.settings')
 django.setup()
 
-
 from .models import Author, Tag, Quote
-from .utils.scrape import JSON_FOLDER_PATH, AUTHORS_JSON_PATH, QUOTES_JSON_PATH
+from .utils.scrape import AUTHORS_JSON_PATH, QUOTES_JSON_PATH
 
 
 def add_authors():
-    with open(r'C:\GHProjects\Django_quotes\project_quotes\quotes\utils\authors.json', 'r', encoding='utf-8') as file:
+    with open(AUTHORS_JSON_PATH, 'r', encoding='utf-8') as file:
         authors = json.load(file)
         for author in authors:
             author_object, created = Author.objects.get_or_create(
@@ -25,28 +24,26 @@ def add_authors():
 
 
 def add_quotes():
-    with open(r'C:\GHProjects\Django_quotes\project_quotes\quotes\utils\quotes.json', 'r', encoding='utf-8') as file:
+    with open(QUOTES_JSON_PATH, 'r', encoding='utf-8') as file:
         quotes = json.load(file)
     for quote in quotes:
-        try:
-            author_obj = Author.objects.get(fullname=quote.get('author'))
-        except MultipleObjectsReturned as error:
-            print(error)
-            author_obj = None
-        tags_objects = []
-        for tag in quote.get('tags'):
-            tag_obj, created = Tag.objects.get_or_create(name=tag)
-            if not created:
-                tag_obj.save()
-                tags_objects.append(tag_obj)
-        if author_obj:
-            quote_obj, created = Quote.objects.get_or_create(quote=quote.get('quote'), author=author_obj)
-            if not created:
-                for tag in tags_objects:
-                    quote_obj.tags.add(tag)
-                quote_obj.save()
+        tags = []
+        for tag in quote['tags']:
+            t, *_ = Tag.objects.get_or_create(name=tag)
+            tags.append(t)
+
+        exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
+        if not exist_quote:
+
+            a = Author.objects.get(fullname=quote['author'])
+            q = Quote.objects.create(
+                quote=quote['quote'],
+                author=a
+            )
+            for tag in tags:
+                q.tags.add(tag)
 
 
 if __name__ == '__main__':
-    # add_authors()
+    add_authors()
     add_quotes()
