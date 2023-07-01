8c87f60060635c579e53dd5b0613c9f66b3b9539
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-02 00:40:11 +0200

    Update views.py

--- project_quotes/quotes/views.py
+++ project_quotes/quotes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import AuthorForm, QuoteForm, TagForm
 
