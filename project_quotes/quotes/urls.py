31ab406634aab1f2911a4c21bd0cfadb64b2978c
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-04 16:59:10 +0200

    Update urls.py

--- project_quotes/quotes/urls.py
+++ project_quotes/quotes/urls.py
@@ -5,7 +5,9 @@ app_name = 'quotes'
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path("<int:page>", views.main, name='root_paginate'),
     path('tag/', views.add_tag, name='add_tag'),
     path('author/', views.add_author, name='add_author'),
+    path('author/<int:author_id>', views.author_detail, name='author_detail'),
     path('quote/', views.add_quote, name='add_quote'),
 ]
