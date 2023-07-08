5015296aa124688ab8caaeba15ae2e67cb057ee4
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-07-08 12:30:51 +0200

    Update views.py

--- project_quotes/users/views.py
+++ project_quotes/users/views.py
@@ -1,9 +1,21 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 from .forms import RegisterForm, LoginForm
+
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'users/password_reset.html'
+    email_template_name = 'users/password_reset_email.html'
+    html_email_template_name = 'users/password_reset_email.html'
+    success_url = reverse_lazy('users:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'users/password_reset_subject.txt'
 
 
 # Create your views here.
